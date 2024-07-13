754ff03769ecefedfd9bf95cd75f1b738585f64b
at-sacai <1216.sacai@gmail.com>
2024-07-13 23:43:54 +0900

    「在庫管理」部分を修正する

--- vending_machine.rb
+++ vending_machine.rb
@@ -1,92 +1,39 @@
-class Suica
-  DEPOSIT = 500
+class VendingMachine
+  attr_reader :sales
+
   def initialize
-    @charge_fare = DEPOSIT
-  end
+    @sales = 0
+    @price = { 'pepsi' => 150, 'monster' => 250, 'irohasu' => 120 }
+    @stock = { 'pepsi' => [], 'monster' => [], 'irohasu' => [] }
 
-  attr_reader :charge_fare
-
-  def show_charge_fare
-    @charge_fare
-  end
-
-  def add_charge_fare(money)
-    if money < 100
-      puts '最低チャージ可能金額は、100円からになります。'
-    else
-      @charge_fare += money
+    5.times do
+      @stock['pepsi'] << Drink.new('pepsi', 150)
+      @stock['monster'] << Drink.new('monster', 250)
+      @stock['irohasu'] << Drink.new('irohasu', 120)
     end
   end
 
-  def minus_charge_fare(money)
-    @charge_fare -= money
-  end
-end
-
-class VendingMachine
-  def initialize
-    @sales = 0
-    @menu = { 'pepsi' => 150, 'monster' => 250, 'irohasu' => 120 }
-    @stock = { 'pepsi' => 5, 'monster' => 5, 'irohasu' => 5 }
-  end
-
-  def show_revenues
-    @sales
-  end
-
   def show_stock
-    puts "・pepsiの在庫：#{@stock['pepsi']}個"
-    puts "・monsterの在庫：#{@stock['monster']}個"
-    puts "・irohasuの在庫：#{@stock['irohasu']}個"
+    puts "pepsiの在庫は#{@stock['pepsi'].size}個です"
+    puts "monsterの在庫は#{@stock['monster'].size}個です"
+    puts "irohasuの在庫は#{@stock['irohasu'].size}個です"
   end
 
   def add_stock(drink)
-    @stock[drink.drink_name] += 1
+    @stock[drink.name] << drink
   end
 
-  def buy_drink(suica, order)
-    if @stock[order].positive?
-      if suica.charge_fare >= @menu[order]
-        @sales += @menu[order]
-        @stock[order] -= 1
-        suica.minus_charge_fare(@menu[order])
+  def buy(suica, drink)
+    if @stock[drink].size > 0
+      if suica.deposit >= @price[drink]
+        @sales += @price[drink]
+        @stock[drink].shift
+        suica.pay(@price[drink])
       else
         puts 'チャージ残高が足りません'
       end
     else
-      puts "#{order}の在庫がありません"
+      puts "#{drink}の在庫がありません"
     end
   end
 end
-
-class Drink
-  def initialize(drink_name, drink_fare)
-    @drink_name = drink_name
-    @drink_fare = drink_fare
-  end
-
-  attr_reader :drink_name, :drink_fare
-end
-
-suica = Suica.new
-puts suica.add_charge_fare(1000)
-puts suica.show_charge_fare
-
-v_machine = VendingMachine.new
-v_machine.buy_drink(suica, 'pepsi')
-v_machine.buy_drink(suica, 'pepsi')
-v_machine.buy_drink(suica, 'pepsi')
-v_machine.buy_drink(suica, 'pepsi')
-v_machine.buy_drink(suica, 'pepsi')
-v_machine.buy_drink(suica, 'pepsi')
-v_machine.buy_drink(suica, 'monster')
-v_machine.buy_drink(suica, 'monster')
-
-puts v_machine.show_revenues
-pepsi = Drink.new('pepsi', 150)
-monster = Drink.new('monster', 250)
-v_machine.add_stock(pepsi)
-v_machine.add_stock(monster)
-v_machine.show_stock
-puts v_machine.show_revenues
-puts suica.show_charge_fare
